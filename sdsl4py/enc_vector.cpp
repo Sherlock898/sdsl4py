df772d56d5fb9a837fe37708b7604084c4a02aee
Sherlock898 <aguayo.sebastian.r@gmail.com>
2023-12-06 02:08:18 -0300

    Linked default dac_vector

--- sdsl4py/enc_vector.cpp
+++ sdsl4py/enc_vector.cpp
@@ -8,6 +8,9 @@
 namespace py = pybind11;
 
 using sdsl::enc_vector;
+using sdsl::vlc_vector;
+using sdsl::dac_vector;
+
 
 template <class T>
 inline auto add_enc_vector(py::module &m, const char* name){
@@ -15,7 +18,7 @@ inline auto add_enc_vector(py::module &m, const char* name){
     m.def("size_in_bytes", &sdsl::size_in_bytes<T>);
     m.def("size_in_mega_bytes", &sdsl::size_in_mega_bytes<T>);
 
-    py::class_<T>(m, name)
+    return py::class_<T>(m, name)
     .def(py::init([](const sdsl::int_vector<0> container){
         return new T(container);
     }), py::arg("container"))
@@ -31,21 +34,31 @@ inline auto add_enc_vector(py::module &m, const char* name){
     })
     //TODO: swap method
     //TODO: load, serialize
-    .def("sample", &T::sample)
-    .def("get_sample_dens", &T::get_sample_dens)
+    //TODO: sample
     //TODO: get_inter_sampled_values
     .def("__iter__", [](const py::sequence &self){
             return py::make_iterator(self.begin(), self.end());
         },
-        py::keep_alive<0, 1>(), py::is_operator());
+    py::keep_alive<0, 1>(), py::is_operator());
+}
+
+template <class T>
+inline auto add_enc_vlc_vector(py::module &m, const char* name){
+    add_enc_vector<T>(m, name).def("get_sample_dens", &T::get_sample_dens);
 }
 
 void enc_vector_wrapper(py::module &m){
     //TODO: Support variable density
-    add_enc_vector<enc_vector<sdsl::coder::elias_delta, 128, 0>>(m, "enc_vector_elias_delta");
-    add_enc_vector<enc_vector<sdsl::coder::elias_gamma, 128, 0>>(m, "enc_vector_elias_gamma");
-    add_enc_vector<enc_vector<sdsl::coder::fibonacci, 128, 0>>(m, "enc_vector_fibonacci");
-    add_enc_vector<enc_vector<sdsl::coder::comma<2>, 128, 0>>(m, "enc_vector_comma_2");
+    add_enc_vlc_vector<enc_vector<sdsl::coder::elias_delta, 128, 0>>(m, "enc_vector_elias_delta");
+    add_enc_vlc_vector<enc_vector<sdsl::coder::elias_gamma, 128, 0>>(m, "enc_vector_elias_gamma");
+    add_enc_vlc_vector<enc_vector<sdsl::coder::fibonacci, 128, 0>>(m, "enc_vector_fibonacci");
+    add_enc_vlc_vector<enc_vector<sdsl::coder::comma<2>, 128, 0>>(m, "enc_vector_comma_2");
+
+    add_enc_vlc_vector<vlc_vector<sdsl::coder::elias_delta, 128, 0>>(m, "vlc_vector_elias_delta");
+    add_enc_vlc_vector<vlc_vector<sdsl::coder::elias_gamma, 128, 0>>(m, "vlc_vector_elias_gamma");
+    add_enc_vlc_vector<vlc_vector<sdsl::coder::comma<2>, 128, 0>>(m, "vlc_vector_comma_2");
+
+    add_enc_vector<dac_vector<>>(m, "dac_vector");
 }
 
 
